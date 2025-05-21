1ba220764df9863aa51955a54e42f7a44dea77c0
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-21 08:42:45 +0530

    Create 02_Name.java
    Sending a distress call using Java

---
+++ Daily Learning/Day1: 21-05-2025/02_Name.java
@@ -0,0 +1,7 @@
+// A simple Java program to print out my name and send distress call.
+
+public class Name{
+    public static void main(String[] args) {
+        System.out.println("My Name is Ayan Khan, If anyone is listening to this distress call, Please respond!");
+    }
+}
