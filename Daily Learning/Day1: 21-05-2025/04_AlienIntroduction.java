0380b12ac030c73ee2b03792410dacd497a115e6
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-21 08:54:51 +0530

    Create 04_AlienIntroduction.java

---
+++ Daily Learning/Day1: 21-05-2025/04_AlienIntroduction.java
@@ -0,0 +1,8 @@
+// A simple Intrcdution to the unknown world
+
+public class AlienIntroduction {
+    public static void main(String[] args) {
+        System.out.println("Greetigns from Ayan Khan. Coordinates: X:123, Y:456");
+        System.out.println("Mission Objectives: Survey this planet and Learn Java");
+    }
+}
