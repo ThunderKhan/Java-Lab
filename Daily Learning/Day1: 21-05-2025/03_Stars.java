c770bf07e97ee1c43f2272eb1f27f8b6fce7b6bd
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-21 08:48:24 +0530

    Create 03_Stars.java

---
+++ Daily Learning/Day1: 21-05-2025/03_Stars.java
@@ -0,0 +1,10 @@
+// A simple Java program to printout a star map.
+
+public class Stars{
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
