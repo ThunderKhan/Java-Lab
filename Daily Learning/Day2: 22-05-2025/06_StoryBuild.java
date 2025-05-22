adb1b579b7201a3b43c6d29c5ecde98fcdd095fe
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-22 09:36:54 +0530

    Create 06_StoryBuild.java

---
+++ Daily Learning/Day2: 22-05-2025/06_StoryBuild.java
@@ -0,0 +1,19 @@
+// A simple java program to continue the learning story
+
+public class StoryBuild {
+    public static void main(String[] args) {
+        
+        //Declare Variables
+        String pilotName = "Ayan Khan";
+        String planetName = "Zeta-9";
+        double atmosphericPressure = 107.79;
+        boolean lifeFound = false;
+
+        // Transmitting Message
+        System.out.println("TRANSMISSION BEGAN");
+        System.out.println("\n\nCaptain " + pilotName + " has landed on the planet " + planetName + ".");
+        System.out.println("Atmospheric Pressure: " + atmosphericPressure + " kPa.");
+        System.out.println("Is life detected? " + lifeFound + ".");
+        System.out.println("\n\nEND OF TRANSMISSION");
+    }
+}
