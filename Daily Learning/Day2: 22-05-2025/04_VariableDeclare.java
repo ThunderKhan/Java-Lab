a082edb1c921c2ce96da7b3903ef6ddc72b1ace3
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-22 09:20:14 +0530

    Create 04_VariableDeclare.java

---
+++ Daily Learning/Day2: 22-05-2025/04_VariableDeclare.java
@@ -0,0 +1,21 @@
+// A simple java program to declare variables and assign values later.
+
+public class VariableDeclare {
+    public static void main(String[] args) {
+        String alienSpecies;
+        int eggsFound;
+        double averageEggMass;
+
+        alienSpecies = "Zorgon";
+        eggsFound = 42;
+        averageEggMass = 3.14;
+
+        System.out.println("BEGIN TRANSMISSION");
+        System.out.println("Non-Intelligent life form found!");
+        System.out.println("\n\nDATA LOADING...");
+        System.out.println("\n\nSpeices name: " + alienSpecies);
+        System.out.println("No. of Eggs found: " + eggsFound);
+        System.out.println("Average egg Mass: " + averageEggMass);
+        System.out.println("\n\nENDING TRANSMISSION");
+    }
+}
