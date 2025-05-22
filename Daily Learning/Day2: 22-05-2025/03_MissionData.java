89a2ff6addbe74e593fe89625af8eff897b50808
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-22 07:51:44 +0530

    Create 03_MissionData.java

---
+++ Daily Learning/Day2: 22-05-2025/03_MissionData.java
@@ -0,0 +1,11 @@
+// A simple Java program to display mission data.
+
+public class MissionData {
+    public static void main(String[] args) {
+        String pilot = "Ayan Khan";
+        int missionDay = 2;
+        double signalStrength = 98.6;
+
+        System.out.println("\nMission Pilot:\t" + pilot + "\nMission Day:\t" + missionDay + "\nSignal Strength:\t" + signalStrength + " Hz");
+    }
+}
