53f70391dafc585b16e7e8d6a94c6f4e196cddb4
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-22 06:43:59 +0530

    Create 01_DataTest.java

---
+++ Daily Learning/Day2: 22-05-2025/01_DataTest.java
@@ -0,0 +1,15 @@
+// A Simple Java program to print various data using different primitive and non-primitive data types
+
+public class DataTest {
+    public static void main(String[] args) {
+        String name = "Ayan Khan";
+        int age = 21;
+        double temperature = 287.67;
+        boolean intelligentLife = false;
+
+        System.out.println("Name:\t" + name);
+        System.out.println("Age:\t" + age);
+        System.out.println("Planet Temperature:\t" + temperature + " K");
+        System.out.println("Intelligent life found:\t" + intelligentLife);
+    }
+}
