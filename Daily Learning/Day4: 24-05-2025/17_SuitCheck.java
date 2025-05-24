3e5bd2dabe96783b643e15d8a2a547d1ecc0b697
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-25 00:21:05 +0530

    Create 17_SuitCheck.java

---
+++ Daily Learning/Day4: 24-05-2025/17_SuitCheck.java
@@ -0,0 +1,7 @@
+public class SuitCheck {
+    public static void main(String[] args) {
+        for (int timeSeconds = 5; timeSeconds <= 30; timeSeconds += 5) {
+            System.out.println("Suit check in: " + timeSeconds + " seconds");
+        }
+    }
+}
