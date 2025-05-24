666b6825b408604a4b7c5e9b68d38775dc68e24d
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-24 12:14:52 +0530

    Update 02_LiftOff.java
    Upgraded

--- Daily Learning/Day4: 24-05-2025/02_LiftOff.java
+++ Daily Learning/Day4: 24-05-2025/02_LiftOff.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class LiftOff {
     public static void main(String[] args) {
-        int countDown = 10;
 
-        System.out.println("Launch in");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Start the Launch Sequence? (yes/no): ");
+        String launchResponse  = input.nextLine();
+
+        System.out.print("Enter security code: ");
+        String launchSecurityCode = input.nextLine();
+
+        if (!launchResponse.equalsIgnoreCase("yes") || !launchSecurityCode.equalsIgnoreCase("SEC-ZAIK-42")) {
+            System.out.println("Launch sequence aborted.");
+            return;
+        }
+
+        int countDown = 10;
+        System.out.println("Security code verified.");
+        System.out.println("Welcome commander Khan!");
+        System.out.println("Launch sequence initiated.");
+        System.out.println("LAUNCH IN");
         while (countDown > 0) {
-            System.out.print("\n\nT-minus " + countDown + "... ");
+            System.out.print("\nT-minus " + countDown + "... ");
             
             try {
                 Thread.sleep(1000); // Wait for 1 second (1000 milliseconds)
@@ -14,6 +32,8 @@ public class LiftOff {
             }
             countDown--;
         }
-        System.out.println("\n\n Liftoff!");
+        System.out.println("\n\nLiftoff!");
+
+        input.close();
     }
 }
