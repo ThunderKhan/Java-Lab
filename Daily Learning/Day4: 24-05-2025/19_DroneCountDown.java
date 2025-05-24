57a5e6c120fc7d26480e4c2bf3b8ade593ae614f
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-25 00:30:49 +0530

    Create 19_DroneCountDown.java

---
+++ Daily Learning/Day4: 24-05-2025/19_DroneCountDown.java
@@ -0,0 +1,8 @@
+public class DroneCountDown {
+    public static void main(String[] args) {
+        for (int i = 10; i >0; i--) {
+            System.out.println("T-minus " + i + " seconds");
+        }
+        System.out.println("Liftoff!");
+    }
+}
