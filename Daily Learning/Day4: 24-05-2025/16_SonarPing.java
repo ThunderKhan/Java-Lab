1157d7199cd1d7ab68a7bb1d82c5440e5949a531
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-24 23:58:05 +0530

    Create 16_SonarPing.java

---
+++ Daily Learning/Day4: 24-05-2025/16_SonarPing.java
@@ -0,0 +1,7 @@
+public class SonarPing {
+    public static void main(String[] args) {
+        for (int angel = 1; angel < 20; angel += 2) {
+            System.out.println("Scanning at angel: " + angel + " degrees");
+        }
+    }
+}
