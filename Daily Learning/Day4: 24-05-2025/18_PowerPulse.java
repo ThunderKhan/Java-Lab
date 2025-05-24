6a32a80e65308c2f6f337e5e6f38351b4059fc5d
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-25 00:26:26 +0530

    Create 18_PowerPulse.java

---
+++ Daily Learning/Day4: 24-05-2025/18_PowerPulse.java
@@ -0,0 +1,7 @@
+public class PowerPulse {
+    public static void main(String[] args) {
+        for (int pulse = 10; pulse <= 100; pulse += 10) {
+            System.out.println("Pulse strength: " + pulse + "%");
+        }
+    }
+}
