3a022b1bb413bc4bf90aefd5da435a2db5dcc3a2
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-24 23:51:50 +0530

    Create 14_EnergyPackets.java

---
+++ Daily Learning/Day4: 24-05-2025/14_EnergyPackets.java
@@ -0,0 +1,8 @@
+public class EnergyPackets {
+    public static void main(String[] args) {
+
+        for (int distance = 10; distance <= 60; distance += 10) {
+            System.out.println("Drop energy packets at " + distance + " meters.");
+        }
+    }
+}
