e170dd168639db771d4f1c33c7f9c501f42ea017
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-24 23:54:41 +0530

    Create 15_DescentBoost.java

---
+++ Daily Learning/Day4: 24-05-2025/15_DescentBoost.java
@@ -0,0 +1,8 @@
+public class DescentBoost {
+    public static void main(String[] args) {
+
+        for (int thrust = 100; thrust >=0; thrust -= 10) {
+            System.out.println("Engine thrust: " + thrust + "%");
+        }
+    }
+}
