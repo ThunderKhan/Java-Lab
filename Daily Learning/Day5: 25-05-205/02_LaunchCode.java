ff767b692b885c281b1886d196474abd42327bc2
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-25 11:26:51 +0530

    Create 02_LaunchCode.java

---
+++ Daily Learning/Day5: 25-05-205/02_LaunchCode.java
@@ -0,0 +1,9 @@
+public class LaunchCode {
+    public static void main(String[] args) {
+        System.out.println("Launch Code: " + getLaunchCode());
+    }
+
+    static String getLaunchCode() {
+        return "ZK-92-PHX";
+    }
+}
