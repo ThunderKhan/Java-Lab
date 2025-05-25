cda57e490a54abbbb42204663a66e247382b35ff
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-05-25 11:18:17 +0530

    Create 01_MissionControl.java

---
+++ Daily Learning/Day5: 25-05-205/01_MissionControl.java
@@ -0,0 +1,9 @@
+public class MissionControl {
+    public static void main(String[] args) {
+        displaySystemStatus();
+    }
+
+    static void displaySystemStatus() {
+        System.out.println("\n\nStatus: All systems nominal. Reactor core stable. Orbit sync achieved.");
+    }
+}
