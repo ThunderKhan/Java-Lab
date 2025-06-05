2eb0505b9a5c142143b896475098765f288c1e7b
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2025-06-05 05:49:26 +0530

    Create 02_TestArray2.java

---
+++ Daily Learning/Day7: 05-06-2025/02_TestArray2.java
@@ -0,0 +1,11 @@
+public class TestArray2 {
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple", "Banana", "Pear", "Orange"};
+
+        // Advanced for loop
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
